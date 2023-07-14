96d0866a21622f01ab63a6182700bad6e623a6cc
Xinyue Li <xili@c3r6p6.42vienna.com>
2023-07-14 11:55:34 +0200

    c04 unfinished

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: xili <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:23:29 by xili              #+#    #+#             */
-/*   Updated: 2023/07/11 19:44:02 by xili             ###   ########.fr       */
+/*   Updated: 2023/07/13 22:14:46 by xili             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -15,25 +15,38 @@ void	ft_putnbr(int nb)
 {
 	char narr[12];
 	int	i;
-	int	tempn;
-
+	long long	tempn;
+	if (nb == 0)
+	{
+		write(1, "0", 1)
+		return ;
+	}
 	if (nb < 0)
 	{
 		write(1, "-", 1);
 		nb = -nb;
 	}
 	i = 0;
-	while (nb > 0)
+	if (nb > 0)
 	{	
-		tempn = nb % 10;
-		narr[i] = tempn - '0';
-		i++;
+		while (nb >= 0)
+		{
+			nb = nb % 10;
+			narr[i] = nb % 10  - '0';
+			i++;
+		}
 	}
 	narr[i] = nb - '0';
-	while (i > 0)
+	*ptr_i = &i;
+	reverse_narr(narr[]
+	reverse_narr(narr[],,
+}
+void reverse_narr(int *narr, int *i)
+{
+	while (*i > 0)
 	{	
-		i--;
-		write (1, narr[i],1);
+		*i--;
+		write (1, narr[*i],1);
 	}
 }
 
